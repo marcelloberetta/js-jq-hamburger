42e0dbd9ab1668813eacc7011a926fd2439fc9a1
marcelloberetta <marcelloberetta4@gmail.com>
2021-04-19 16:56:43 +0200

    Esercizio DONE

---
+++ main.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+
+var icona = $(".header-right a");
+
+var hamburgerMenu = $(" .hamburger-menu");
+
+var iconaX = $(".hamburger-menu .close");
+
+//FUNZIONI
+
+icona.click(function(){
+   hamburgerMenu.show();
+})
+
+iconaX.click(function(){
+   hamburgerMenu.hide();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
